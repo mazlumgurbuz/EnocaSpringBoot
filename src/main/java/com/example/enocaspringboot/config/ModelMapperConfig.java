04a76a6cbb72dc94883b15c55d66a2fc99795489
Mazlum23 <mazlumgurbuz30@gmail.com>
2022-05-13 15:22:01 +0300

    Update ModelMapperConfig.java

--- src/main/java/com/example/enocaspringboot/config/ModelMapperConfig.java
+++ src/main/java/com/example/enocaspringboot/config/ModelMapperConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ModelMapperConfig {
-
     @Bean
     public ModelMapper mapper() {
         return new ModelMapper();
